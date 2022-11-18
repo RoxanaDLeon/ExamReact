62e9e34bf251fb33f2b0988cfbcd510e45423a9f
RoxanaDLeon <116852269+RoxanaDLeon@users.noreply.github.com>
2022-11-18 10:03:53 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AddUser from './Components/Users/AddUser';
+import AddItem from './Components/Users/AddItem';
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser/>
+      <AddItem/>
+    </div>
+  );
+}
+
+export default App;
